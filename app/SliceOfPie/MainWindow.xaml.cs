c91b3c8fd3432f6abae5e0ad418ed1e0d14a8e7a
Niclas Tollstorff <niclastollstorff@gmail.com>
2012-11-28 00:15:46 +0100

    Added icons to the Document explorer programatically. Also added a helper method for creating items in the Document explorer

--- app/SliceOfPie/MainWindow.xaml.cs
+++ app/SliceOfPie/MainWindow.xaml.cs
@@ -17,11 +17,32 @@ namespace SliceOfPie {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        private ContextMenu projectContextMenu;
-        private ContextMenu folderContextMenu;
-        private ContextMenu documentContextMenu;
+        private ContextMenu projectContextMenu, folderContextMenu, documentContextMenu;
+        private BitmapImage projectIcon, folderIcon, documentIcon;
+
         public MainWindow() {
             InitializeComponent();
+            InitializeDocumentExplorer();
+            RefreshDocumentExplorer();
+        }
+
+        private void InitializeDocumentExplorer() {
+            //Setup icons
+            projectIcon = new BitmapImage();
+            projectIcon.BeginInit();
+            projectIcon.UriSource = new Uri("pack://application:,,,/Icons/project-icon.bmp");
+            projectIcon.EndInit();
+
+            folderIcon = new BitmapImage();
+            folderIcon.BeginInit();
+            folderIcon.UriSource = new Uri("pack://application:,,,/Icons/folder-icon.bmp");
+            folderIcon.EndInit();
+
+            documentIcon = new BitmapImage();
+            documentIcon.BeginInit();
+            documentIcon.UriSource = new Uri("pack://application:,,,/Icons/document-icon.bmp");
+            documentIcon.EndInit();
+
             //Setup Project Context Menu for the Document Explorer
             projectContextMenu = new ContextMenu();
             projectContextMenu.Items.Add(new MenuItem() { Header = "Share project" });
@@ -70,7 +91,48 @@ namespace SliceOfPie {
             }
         }
 
-        // private void RefreshDocumentExplorer() {
+        /// <summary>
+        /// This is a helper method for creating an item in the document explorer.
+        /// </summary>
+        /// <param name="text">The text to be shown in the item</param>
+        /// <param name="itemType">The type of the item. Can be either "project", "folder", or "document"</param>
+        /// <returns></returns>
+        private TreeViewItem createDocumentExplorerItem(string text, string itemType) {
+            TreeViewItem item = new TreeViewItem() {Tag = itemType };
+            //StackPanel for image and text block
+            StackPanel sp = new StackPanel() { Orientation = Orientation.Horizontal };
+            //Create the image
+            BitmapImage icon;
+            if (itemType.Equals("project")) {
+                icon = projectIcon;
+            }
+            else if (itemType.Equals("folder")) {
+                icon = folderIcon;
+            }
+            else {
+                icon = documentIcon;
+            }
+            Image image = new Image() { Source = icon, Height = 15, Width = 15, IsHitTestVisible = false }; /* note that IsHitTestVisible=false disables event handling for this element -
+                                                                                                             * fallback on the general treeview handling (rightclick for the context menu).
+                                                                                                             */
+            sp.Children.Add(image);
+            //Create the text block
+            TextBlock itemText = new TextBlock() { Text = text, Margin = new Thickness(5, 0, 0, 0), IsHitTestVisible = false };
+            sp.Children.Add(itemText);
+            item.Header = sp;
+
+            return item;
+        }
+
+        /// <summary>
+        /// This method is supposed to refresh the document explorer.
+        /// It exists as a placeholder and testing method untill the model/controller allows local file traversal.
+        /// </summary>
+        private void RefreshDocumentExplorer() {
+            TreeViewItem t = createDocumentExplorerItem("test project", "project");
+            t.Items.Add(createDocumentExplorerItem("test folder", "folder"));
+            t.Items.Add(createDocumentExplorerItem("test document", "document"));
+            DocumentExplorer.Items.Add(t);
 
         //    //var projects = Controller.getprojects();
         //    DocumentExplorer.Items.Clear();
@@ -80,7 +142,7 @@ namespace SliceOfPie {
         //        TreeViewItem project = new TreeViewItem() { Header=p.title, Tag="project" //Project node
         //        DocumentExplorer.Items.Add(TreeViewItem);
         //    }
-        //}
+        }
     }
 
 }
