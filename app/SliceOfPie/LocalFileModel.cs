80395c94cef9993b5411cce640f3d496c4e75310
Michael Storgaard <github@michaelstorgaard.com>
2012-11-27 16:27:51 +0100

    added partials with interfaces and some of file model

--- app/SliceOfPie/LocalFileModel.cs
+++ app/SliceOfPie/LocalFileModel.cs
@@ -5,6 +5,36 @@ using System.Text;
 
 namespace SliceOfPie {
     public class LocalFileModel : FileModel {
+        public static List<Project> GetProjects(int userId) {
+            List<Project> projects = new List<Project>();
 
+            return projects;
+        }
+
+        public static IEnumerable<Item> ListProject(Project project) {
+            List<Item> items = new List<Item>();
+
+            items.Add(new Folder());
+            items.Add(new Document());
+
+            return items;
+        }
+
+        public static IEnumerable<Item> ListFolder(Folder folder) {
+            List<Item> items = new List<Item>();
+
+            items.Add(new Folder());
+            items.Add(new Document());
+
+            return items;
+        }
+
+        public static void SaveDocument(Document doc) {
+            throw new NotImplementedException();
+        }
+
+        public static Document LoadDocument(int docId) {
+            throw new NotImplementedException();
+        }
     }
 }
