4b639a353c9a234b44bc79377d5deda375bfc259
Michael Storgaard <github@michaelstorgaard.com>
2012-11-27 13:55:23 +0100

    added basic files

---
+++ app/SliceOfPie/FileModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SliceOfPie {
+    public abstract class FileModel {
+        public abstract void SaveDocument(Document doc);
+        public abstract Document LoadDocument(int docId);
+    }
+}
