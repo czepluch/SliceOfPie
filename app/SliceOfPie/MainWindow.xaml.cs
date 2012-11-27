b1df2b5eb397cddde081d7e30049302eb41bf65a
Niclas Tollstorff <niclastollstorff@gmail.com>
2012-11-27 17:32:08 +0100

    Added basic WPF window with a text editor

--- app/SliceOfPie/MainWindow.xaml.cs
+++ app/SliceOfPie/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace SliceOfPie
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
