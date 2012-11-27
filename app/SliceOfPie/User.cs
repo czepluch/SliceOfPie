b0f4e5f285a0375d38e9a85cedb78f44039f18da
unknown <sigurt91@gmail.com>
2012-11-27 16:36:43 +0100

    modified:   app/SliceOfPie/Controller.cs 	new file:   app/SliceOfPie/User.cs

---
+++ app/SliceOfPie/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SliceOfPie {
+    partial class User {
+        private static User _local = new User();
+
+        public User Local {
+            get {
+                return _local;
+            }
+        }
+    }
+}
