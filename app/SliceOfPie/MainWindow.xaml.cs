54fc6ae1ba6bb1481cadc9368cfa814afad72033
Niclas Tollstorff <niclastollstorff@gmail.com>
2012-11-27 22:03:30 +0100

    Added a dynamic context menu to the document explorer

--- app/SliceOfPie/MainWindow.xaml.cs
+++ app/SliceOfPie/MainWindow.xaml.cs
@@ -12,23 +12,64 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SliceOfPie
-{
+namespace SliceOfPie {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+            //RefreshDocumentExplorer();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
+        /// <summary>
+        /// This generates and shows a context menu for the document explorer at runtime
+        /// </summary>
+        /// <param name="item">The item which was clicked on the document explorer</param>
+        private void generateFolderContextMenu(TreeViewItem item) {
+            ContextMenu c = new ContextMenu();
+            string itemType = (string)item.Tag;
+            if (itemType.Equals("project")) {
+                c.Items.Add(new MenuItem() { Header = "Share project" });
+                c.Items.Add(new MenuItem() { Header = "Add folder" });
+                c.Items.Add(new MenuItem() { Header = "Add document" });
+            }
+            else if (itemType.Equals("folder")) {
+                c.Items.Add(new MenuItem() { Header = "Add folder" });
+                c.Items.Add(new MenuItem() { Header = "Add document" });
+            }
+            else {
+                c.Items.Add(new MenuItem() { Header = "Edit document" });
+            }
+            DocumentExplorer.ContextMenu = c;
+            DocumentExplorer.ContextMenu.IsOpen = true;
         }
 
+        /// <summary>
+        /// This makes right click select an item in the Document Explorer and show the appropiate context menu for the item.
+        /// </summary>
+        /// <param name="sender">The object that send the event</param>
+        /// <param name="e">The MouseButtonEventArgs for the event</param>
+        private void DocumentExplorer_MouseRightButtonDown(object sender, MouseButtonEventArgs e) {
+            TreeViewItem item = e.Source as TreeViewItem;
+            if (item != null) {
+                item.IsSelected = true;
+                generateFolderContextMenu(item);
+                e.Handled = true;
+            }
+        }
 
+        // private void RefreshDocumentExplorer() {
+
+        //    //var projects = Controller.getprojects();
+        //    DocumentExplorer.Items.Clear();
+        //    foreach (Project p in projects) {
+
+        //item.Tag = "project";
+        //        TreeViewItem project = new TreeViewItem() { Header=p.title, Tag="project" //Project node
+        //        DocumentExplorer.Items.Add(TreeViewItem);
+        //    }
+        //}
     }
+
 }
