ac3663a2c55fb10bf91eea6445f24be6a45bd09d
Bladtman <sigurt91@gmail.com>
2012-12-04 12:21:12 +0100

    Added Merger to fix csproj accident 	new file:   app/SliceOfPie/Merger.cs 	modified:   app/SliceOfPie/SliceOfPie.csproj

---
+++ app/SliceOfPie/Merger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SliceOfPie {
+    class Merger {
+        private Merger() {
+        }
+
+        public static Document Merge(Document current, Document old) {
+            String[] oldArr = current.CurrentRevision.Split('\n');
+            Console.WriteLine(string.Join("{0}", oldArr));
+            return null;
+        }
+
+        public static void Main(String[] args) {
+            Document o = new Document{
+                CurrentRevision = @"DRACULA 
+
+
+
+CHAPTER I 
+
+JONATHAN BARKER'S JOURNAL 
+(Kept in shorthand.) 
+
+3 May. Bistritz. Left Munich at 8:35 p. M., on ist May, ar- 
+riving at Vienna early next morning; should have arrived at 
+6:46, but train was an hour late. Buda-Pesth seems a wonderful 
+place, from the glimpse which I got of it from the train and the 
+little I could walk through the streets. I feared to go very far 
+from the station, as we had arrived late and would start as near 
+the correct time as possible. The impression I had was that we 
+were leaving the West and entering the East; the most western 
+of splendid bridges over the Danube, which is here of noble width 
+and depth, took us among the traditions of Turkish rule. "
+            };
+            Document c = new Document {
+                CurrentRevision = @"DRACULA 
+
+
+
+CHAPTER I 
+
+JONATHAN BARKER'S JOURNAL 
+(Kept in shorthand.) 
+
+3 May. Bistritz. Left Munich at 8:35 p. M., on ist May, ar- 
+riving at Vienna early next morning; should have arrived at 
+6:46, but train was an hour late. Buda-Pesth seems a wonderful 
+place, from the glimpse which I got of it from the train and the 
+little I could walk through the streets. I feared to go very far 
+from the station, as we had arrived late and would start as near 
+the correct time as possible. The impression I had was that we 
+were leaving the West and entering the East; the most western 
+of splendid bridges over the Danube, which is here of noble width 
+and depth, took us among the traditions of Turkish rule. "
+            };
+
+            Console.WriteLine(c);
+        }
+    }
+}
