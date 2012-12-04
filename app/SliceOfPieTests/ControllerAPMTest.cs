9013d04c1db2876a0781f42e3eda5ec004c024e0
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-12-04 18:01:11 +0100

    CSprojFile

---
+++ app/SliceOfPieTests/ControllerAPMTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SliceOfPie;
+
+namespace SliceOfPieTests {
+    [TestClass]
+    public class ControllerAPMTest {
+        private Controller controller = Controller.Instance;
+
+        /// <summary>
+        /// Tests that projects may be created asynchronously
+        /// </summary>
+        [TestMethod]
+        public void TestProjectCreate() {
+            string folderName = "New Project";
+            IAsyncResult ar = controller.BeginCreateProject(folderName, "user@mail.com", (a) => {
+                //Project p = controller.EndCreateProject(ar);
+            }, new object());
+        }
+    }
+}
