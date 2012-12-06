8c7e1e58f675f287bdacbadb406a304b435230ca
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2012-12-06 01:56:08 +0100

    Made some basic html and added some models to play with

---
+++ app/MvcWebApp/Models/Folder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebApp.Models
+{
+    public class Folder
+    {
+        public int Id { get; set; }
+        public int ProjectID { get; set; }
+        public int FolderID { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ app/MvcWebApp/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebApp.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
