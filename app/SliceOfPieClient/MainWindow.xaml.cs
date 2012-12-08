70d7f4a586877e42aa33e207662af764e7760cb8
Niclas Tollstorff <niclastollstorff@gmail.com>
2012-12-08 13:13:10 +0100

    Removed unused code, now that APM GetProjects works in the UI

--- app/SliceOfPieClient/MainWindow.xaml.cs
+++ app/SliceOfPieClient/MainWindow.xaml.cs
@@ -59,17 +59,10 @@ namespace SliceOfPie.Client {
         /// </summary>
         /// <param name="itemToOpen">The item to open, when the projects are reloaded. Default is the top project.</param>
         private void ReloadProjects(IListableItem itemToOpen = null) {
-            //documentExplorer.Projects = controller.GetProjects("local").ToList();
-            //if (itemToOpen != null) {
-            //    documentExplorer.ExpandTo(itemToOpen, Open);
-            //}
-            //else {
-            //    documentExplorer.CallbackSelected(Open);
-            //}
-
             //Using controllers APM to load the projects into the Document Explorer
             SynchronizationContext context = SynchronizationContext.Current; //Call callback on own thread.
             controller.BeginGetProjects("local", (iar) => {
+                //Callback posted in UI-context
                 context.Post((o) => {
                     documentExplorer.Projects = controller.EndGetProjects(iar).ToList();
                     if (itemToOpen != null) {
