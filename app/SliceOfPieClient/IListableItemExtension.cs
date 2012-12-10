b25334bf5789f864ba71146e5517232df57a23b5
Niclas Tollstorff <niclastollstorff@gmail.com>
2012-12-10 23:51:30 +0100

    Added recursive conflict icon. Projects will show conflicted icon if it contains a conflicted document etc

---
+++ app/SliceOfPieClient/IListableItemExtension.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace SliceOfPie.Client {
+    /// <summary>
+    /// This is a factory for standard icons in the SliceOfPie user interface.
+    /// </summary>
+    public static class IListableItemExtension {
+
+        private static BitmapImage
+            projectIcon = ImageUtil.CreateBitmapImage("/Icons/project-icon.png"),
+            folderIcon = ImageUtil.CreateBitmapImage("/Icons/folder-icon.png"),
+            documentIcon = ImageUtil.CreateBitmapImage("/Icons/document-icon.png"),
+            projectConflictIcon = ImageUtil.CreateBitmapImage("/Icons/project-icon-conflict.png"),
+            folderConflictIcon = ImageUtil.CreateBitmapImage("/Icons/folder-icon-conflict.png"),
+            documentConflictIcon = ImageUtil.CreateBitmapImage("/Icons/document-icon-conflict.png");
+
+        /// <summary>
+        /// Returns the icon for this IListabeItem
+        /// </summary>
+        /// <param name="item">The IListableItem this method is invoked on.</param>
+        /// <returns>The icon of the item</returns>
+        public static BitmapImage GetIcon(this IListableItem item) {
+            if (item is Project) {
+                return item.hasConflict() ? projectConflictIcon : projectIcon;
+            }
+            else if (item is Folder) {
+                return item.hasConflict() ? folderConflictIcon : folderIcon;
+            }
+            else {
+                return item.hasConflict()? documentConflictIcon : documentIcon;
+            }
+        }
+
+        /// <summary>
+        /// Returns whether or not this item is/has a merged document
+        /// </summary>
+        /// <param name="item">The IListableItem this method is invoked on.</param>
+        /// <returns>True if the item is/has a merged document.</returns>
+        private static bool hasConflict(this IListableItem item) {
+            //If this is a document and is set as merged
+            if (item is Document) {
+                return (item as Document).IsMerged;
+            }
+            else { //or if it's a folder containing a document set as merged
+                foreach (Document subDoc in (item as IItemContainer).GetDocuments()) {
+                    if (subDoc.IsMerged) return true;
+                }
+                //or if it contains a folder set as merged (recursively)
+                foreach (IListableItem subFolder in (item as IItemContainer).GetFolders()) {
+                    if (subFolder.hasConflict()) return true;
+                }
+                return false;
+            }
+        }
+    }
+}
